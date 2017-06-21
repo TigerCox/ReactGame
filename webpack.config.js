7a820fd1aa8956204f64fffbba1e164230e5945c
JoJo <joseph.salomone@dealertrack.com>
2017-06-21 15:59:22 -0700

    nitial commit

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+var webpack = require('webpack');
+var path = require('path');
+
+var BUILD_DIR = path.resolve(__dirname, 'build/client');
+var APP_DIR = path.resolve(__dirname, 'src/client');
+
+var config = {
+  entry:  {
+	app: APP_DIR + '/app/index.jsx',
+	'index.html': APP_DIR + '/index.html'
+  },
+  output: {
+    path: BUILD_DIR + '/js',
+    filename: 'bundle.js'
+  },
+  module : {
+    loaders : [
+      { test : /\.jsx$/, include : APP_DIR, loader : 'babel-loader' },
+	  { test : /\.json$/, include : APP_DIR, loader : 'json-loader' },
+	  { test : /\.html$/, include : APP_DIR, loader : 'file-loader?name=[name].[ext]' }
+    ]
+  },
+  devServer: {
+    port: 8888,
+	contentBase: path.join(__dirname, "src/client"),
+    compress: true,
+    host: "localhost"
+  }
+};
+
+module.exports = config;
